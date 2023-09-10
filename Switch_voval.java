088713f3cf7f9e32d627bf1538609bfe2a6f2276
Harsh Tripathi <134940016+Harsh56t@users.noreply.github.com>
2023-09-10 21:34:35 +0530

    Code to check the alphabets in English....

---
+++ Switch_voval.java
@@ -0,0 +1,43 @@
+package addisnal_work;
+import java .util.Scanner;
+public class Switch_voval {
+ 
+
+    public static void main(String[] args) {
+        System.out.println("Enter alfabet to check it is even or odd :-");
+        Scanner sc = new Scanner(System.in);
+      
+        String choice=sc.next();
+        switch(choice){
+         
+         case "a":
+        System.out.println("Your answer is :-");
+       System.out.println("vovel");
+       break;
+         case "e":
+          System.out.println("Your answer is :-");
+       System.out.println("vovel");
+       break;
+         case "i":
+        System.out.println("Your answer is :-");
+       System.out.println("vovel");
+       break;
+         case "o":
+        System.out.println("Your answer is :-");
+       System.out.println("vovel");
+       break;
+       
+         case "u":
+        System.out.println("Your answer is :-");
+       System.out.println("vovel");
+       break;
+       default:
+          System.out.println("Your answer is :-");
+       System.out.println("consonent");
+       break;
+      
+    }
+      
+    }
+}
+
