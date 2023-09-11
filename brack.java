a4a4f17757be13766e99268be616d28033e8ffcc
Harsh Tripathi <134940016+Harsh56t@users.noreply.github.com>
2023-09-11 08:32:39 +0530

    Code of breck and continue statement...

---
+++ brack.java
@@ -0,0 +1,17 @@
+package pak;
+
+public class brack {
+    public static void main(String[] args) {
+        for(int i = 1; i<=10;i++){
+            if(i ==3){
+             //for breck loop we use breck statement...
+             //Code for it is :-
+             //brack;  
+                continue;
+            }
+           System.out.println(i);
+        }
+    }
+}
+
+
